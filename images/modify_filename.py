f075141dd76d7e02ca481f3ac6e88fc72ce07b7f
koumudai <1979938740@qq.com>
2022-03-14 21:51:18 +0800

    Site updated: 2022-03-14 21:50:50

--- images/modify_filename.py
+++ images/modify_filename.py
@@ -2,20 +2,29 @@ import os
 import shutil
 
 def f(path):
+    print(path)
     for e in os.listdir(f'{path}/'):
         if os.path.isdir(e): # 文件夹
             f(f'{path}/{e}')           
         else: # 文件
             if 'svg' in e and e[0] != '.':
                 e_new = e.replace('svg', 'SVG')
-                print(e)
-                print(e_new)
+                print(e, e_new)
                 # shutil.move(f'{path}/{e}', f'{path}/{e_new}')
             elif 'png' in e and e[0] != '.':
                 e_new = e.replace('png', 'PNG')
-                print(e)
-                print(e_new)
+                print(e, e_new)
                 # shutil.move(f'{path}/{e}', f'{path}/{e_new}')
+            elif 'SVG' in e and e[0] != '.':
+                e_new = e.replace('SVG', 'SVG1')
+                print(e, e_new)
+                shutil.move(f'{path}/{e}', f'{path}/{e_new}')
+                shutil.move(f'{path}/{e_new}', f'{path}/{e}')
+            elif 'PNG' in e and e[0] != '.':
+                e_new = e.replace('PNG', 'PNG1')
+                print(e, e_new)
+                shutil.move(f'{path}/{e}', f'{path}/{e_new}')
+                shutil.move(f'{path}/{e_new}', f'{path}/{e}')
             
-
+            
 f('.')
